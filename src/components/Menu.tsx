9a104c2e917dbd4c60d986bd19b9387a1165450a
MiltonLiquinchana <liquinchana@hotmail.es>
2024-06-16 23:37:41 -0500

    se rescribe y restiliza el menu para mejor aspecto

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,52 +1,39 @@
-import { MouseEvent } from "react";
+// import { useRef } from "react";
+import { Link } from "react-router-dom";
 import { ReactSVG } from "react-svg";
 export default function Menu() {
-  const expandir = (e: MouseEvent<HTMLDivElement>): void => {
-    const elemento = e.target as HTMLDivElement;
-    /** Seleccionamos todos los li */
-    const items = elemento.querySelectorAll("li");
-    console.log(items);
-  };
   return (
-    <nav className="nav flex-column navigation navigation-menu">
+    <nav className="navbar bg-body-tertiary">
       <div className="container">
-        <a className="navbar-brand" href="#">
-          <ReactSVG src="src/assets/miLogo.svg" />
-        </a>
-        <div className="menu-header-text">
-          <span>Lorem ipsum</span>
-          <span>Lorem ipsum</span>
+        <div className="navbar-brand">
+          <ReactSVG className="header-menu-logo" src="src/assets/miLogo.svg" />
+          <span className="header-menu-text">San Isidro Aqua</span>
+          <ReactSVG
+            className="header-menu-expand"
+            src="src/assets/menu/short.svg"
+          />
         </div>
-        <button type="button">
-          <ReactSVG src="src/assets/menu/short.svg" />
-        </button>
+        <div className="divider"></div>
+        <nav className="options-menu-list">
+          <div className="expanded">
+            <Link className="expanded-link">
+              <ReactSVG className="expanded-link-icon" src="src/assets/menu/registro.svg" />
+              <span className="expanded-link-text">Lecturas</span>
+              <ReactSVG className="expansion-icon" src="src/assets/menu/expand.svg" />
+            </Link>
+            <nav className="expanded-list">
+              <Link>Todas las lecturasffffe</Link>
+              <Link>Registrar lectura</Link>
+            </nav>
+          </div>
+          <div >
+            <Link className="expanded-link">
+              <ReactSVG className="expanded-link-icon" src="src/assets/menu/home.svg" />
+              <span className="expanded-link-text">Producto</span>
+            </Link>
+          </div>
+        </nav>
       </div>
-      <div className="divider"></div>
-      <a className="nav-link active" aria-current="page" href="#">
-        <ReactSVG src="src/assets/menu/home.svg" />
-        Active
-      </a>
-      <div
-        className="nav-link expanded"
-        tabIndex={0}
-        onClick={(e) => {
-          expandir(e);
-        }}
-        role="button"
-      >
-        <div className="expanded-link">
-          <ReactSVG src="src/assets/menu/registro.svg" />
-          Link
-        </div>
-
-        <ReactSVG className="expanded-icon" src="src/assets/menu/expand.svg" />
-      </div>
-      <a className="nav-link" href="#">
-        Link
-      </a>
-      <a className="nav-link disabled" aria-disabled="true">
-        Disabled
-      </a>
     </nav>
   );
 }
